9b1fa3105e4c0183a649e7e28b88163f0756a8ba
Andre Masson <amwebexpert@gmail.com>
2021-04-09 15:56:41 -0400

    Added static resources like index.html

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,22 @@
 import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 import { AboutModule } from './about/about.module';
 import { databaseConfigs } from './app.configurations';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PreauthMiddleware } from './auth/preauth.moddleware';
 import { CategoriesModule } from './categories/categories.module';
 
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'public') }),
     TypeOrmModule.forRoot(databaseConfigs),
     AboutModule,
     CategoriesModule
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule implements NestModule {
 
