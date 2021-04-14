85ff9dda89714d04387b244d639207381b364070
Andre Masson <amwebexpert@gmail.com>
2021-04-13 20:49:15 -0400

    Added logging

--- startApi.sh
+++ startApi.sh
@@ -1,3 +1,9 @@
-echo Starting localhost in debug mode, watching files
+echo ---------------------------------------------------------------------
+echo FIREBASE environment variables:
 printenv | grep FIREBASE
+echo ---------------------------------------------------------------------
+
+echo ---------------------------------------------------------------------
+echo Starting localhost in debug mode, watching files
+echo ---------------------------------------------------------------------
 npm run start:debug
--- startDb.sh
+++ startDb.sh
@@ -1,3 +1,8 @@
+echo stoping exising local DB (if any)
 docker stop amw-hangman-api_database_1
+
+echo starting local DB
 docker-compose up -d
 docker ps
+
+echo local DB started you can now start the backend API webapp (startApi script)
