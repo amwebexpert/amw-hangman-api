3114c05f4c90d1f39220c95695f36d5ba75d8a4d
Andre Masson <amwebexpert@gmail.com>
2022-06-12 09:54:19 -0400

    feat: enable CORS

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import morganMiddleware from './utils/morgan.middleware';
 import swaggerConfigs from './utils/swagger.configs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   // Swagger setup
   const document = SwaggerModule.createDocument(app, swaggerConfigs);
